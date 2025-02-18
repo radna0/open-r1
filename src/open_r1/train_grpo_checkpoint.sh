d677cb07c3a8ef8334b0ec61ca5a75bd6238ab34
HGB <87762857+radna0@users.noreply.github.com>
2025-02-18 23:33:44 +0700

    Create train_grpo_checkpoint.sh

---
+++ src/open_r1/train_grpo_checkpoint.sh
@@ -0,0 +1,6 @@
+
+
+accelerate launch --config_file=recipes/accelerate_configs/zero3.yaml \
+    --num_processes=2 grpo.py \
+    --config recipes/DeepSeek-R1-Distill-Qwen-1.5B/grpo/config_7b.yaml
+    --resume_from_checkpoint="$1/$2"
