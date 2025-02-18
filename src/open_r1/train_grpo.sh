18b0dc2cb43e729742bc9553b205570acc41eed5
HGB <87762857+radna0@users.noreply.github.com>
2025-02-18 23:37:14 +0700

    Update train_grpo.sh

--- src/open_r1/train_grpo.sh
+++ src/open_r1/train_grpo.sh
@@ -1,5 +1,5 @@
 
 
-accelerate launch --config_file=recipes/accelerate_configs/zero3.yaml \
+accelerate launch --config_file=../../recipes/accelerate_configs/zero3.yaml \
     --num_processes=2 grpo.py \
-    --config recipes/DeepSeek-R1-Distill-Qwen-1.5B/grpo/config_7b.yaml
+    --config ../../recipes/DeepSeek-R1-Distill-Qwen-1.5B/grpo/config_7b.yaml
