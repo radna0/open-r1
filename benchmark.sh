5f2ec8e87849a68b02f39fd085134b2d0a7002cb
HGB <87762857+radna0@users.noreply.github.com>
2025-02-22 21:29:49 +0700

    Create benchmark.sh

---
+++ benchmark.sh
@@ -0,0 +1,17 @@
+python \
+scripts/run_benchmarks.py \
+--model-id=radna/DeepSeek-R1-Distill-Qwen-7B-GRPO-LIMO \
+--model-revision=649fc38e45ec4c2c59a6da532bc2d20b2d697867 \
+--benchmarks=aime24
+
+python \
+scripts/run_benchmarks.py \
+--model-id=radna/DeepSeek-R1-Distill-Qwen-7B-GRPO-LIMO \
+--model-revision=649fc38e45ec4c2c59a6da532bc2d20b2d697867 \
+--benchmarks=math_500
+
+python \
+scripts/run_benchmarks.py \
+--model-id=radna/DeepSeek-R1-Distill-Qwen-7B-GRPO-LIMO \
+--model-revision=649fc38e45ec4c2c59a6da532bc2d20b2d697867 \
+--benchmarks=gpqa
