0f81cf6ad7fedd39dabe7f13c7c12c1b44bed6eb
HGB <87762857+radna0@users.noreply.github.com>
2025-02-22 19:08:10 +0700

    Create eval.sh

---
+++ src/open_r1/eval.sh
@@ -0,0 +1,24 @@
+MODEL=radna/DeepSeek-R1-Distill-Qwen-7B-GRPO-LIMO
+MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.0}"
+OUTPUT_DIR=data/evals/$MODEL
+
+# AIME 2024
+TASK=aime24
+lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+    --custom-tasks evaluate.py \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
+
+# MATH-500
+TASK=math_500
+lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+    --custom-tasks evaluate.py \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
+
+# GPQA Diamond
+TASK=gpqa:diamond
+lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+    --custom-tasks evaluate.py \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR 
